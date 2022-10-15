296088df832ed308789c7067b00afe9948193f6b
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-10-15 23:13:37 +0300

    add axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import axios from "axios";
 
-function App() {
+const FileUploader = ({ onFileSelect }) => {
+  const fileInput = useRef(null);
+
+  const handleFileInput = (e) => {
+    // handle validations
+    onFileSelect(e.target.files[0]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="file-uploader">
+      <input type="file" onChange={handleFileInput} />
+      <button
+        onClick={(e) => fileInput.current && fileInput.current.click()}
+        className="btn btn-primary"
+      />
     </div>
   );
-}
+};
+const App = () => {
+  const UPLOAD_URL="/Users/user/Desktop/form/src/Upload.js"
+  const [name, setName] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const submitForm = () => {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("file", selectedFile);
+
+    axios
+      .post(UPLOAD_URL, formData)
+      .then((res) => {
+        alert("File Upload success");
+      })
+      .catch((err) => alert("File Upload Error"));
+  };
+
+  return (
+    <div className="App">
+      <form>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <FileUploader
+          onFileSelectSuccess={(file) => setSelectedFile(file)}
+          onFileSelectError={({ error }) => alert(error)}
+        />
+
+        <button onClick={submitForm}>Submit</button>
+      </form>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/Upload.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Upload() {
+  return (
+    <div>UPLOAD</div>
+  )
+}
+
+export default Upload;
